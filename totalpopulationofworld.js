002fd52b9f73e7feb146df4b0fb3339833189154
krobingit <91210322+krobingit@users.noreply.github.com>
2021-09-27 19:20:23 +0530

    Add files via upload

---
+++ totalpopulationofworld.js
@@ -0,0 +1,16 @@
+const getData3 = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+    xhr.send();
+    xhr.responseType = "json";
+    xhr.onload = () => {
+
+        const countries = xhr.response;
+
+        var totalpopulation = countries.reduce((total, country) => total + country.population, 0)
+        console.log("Total-Population-of-World: " + totalpopulation) //output-7349137231
+    }
+}
+
+getData3();
+
